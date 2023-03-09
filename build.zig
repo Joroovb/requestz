6e690f09c8b6809ca7a7b6e517effad0dd59a2c6
Joroovb <jorisvanbreugel@mailbox.org>
2023-03-09 11:25:48 +0100

    Added depency to project & updated buildchain to work with zig 0.11

--- build.zig
+++ build.zig
@@ -1,16 +1,34 @@
-const Builder = @import("std").build.Builder;
-const pkgs = @import("deps.zig").pkgs;
+const std = @import("std");
 
-pub fn build(b: *Builder) void {
-    const mode = b.standardReleaseOptions();
-    const lib = b.addStaticLibrary("requestz", "src/main.zig");
-    pkgs.addAllTo(lib);
-    lib.setBuildMode(mode);
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    // Dependencies
+    const http_dep = b.dependency("http", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const http_mod = http_dep.module("http");
+
+    const lib = b.addStaticLibrary(.{
+        .name = "requestz",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    lib.addModule("http", http_mod);
     lib.install();
 
-    var main_tests = b.addTest("src/tests.zig");
-    pkgs.addAllTo(main_tests);
-    main_tests.setBuildMode(mode);
+    const main_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    main_tests.addModule("http", http_mod);
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
