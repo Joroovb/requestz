ab09c24eb7862a404f70e7fef70906c5dd2bf24e
Joroovb <jorisvanbreugel@mailbox.org>
2023-03-09 11:49:13 +0100

    added qol commands and depency

--- build.zig
+++ build.zig
@@ -6,12 +6,29 @@ pub fn build(b: *std.Build) void {
 
     b.addModule(.{ .name = "requestz", .source_file = .{ .path = "src/main.zig" } });
 
+    tests(b, target, optimize);
+    clean(b);
+}
+
+fn tests(b: *std.Build, target: std.zig.CrossTarget, mode: std.builtin.OptimizeMode) void {
     const main_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = .{ .path = "src/test.zig" },
         .target = target,
-        .optimize = optimize,
+        .optimize = mode,
     });
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
+}
+
+fn clean(b: *std.Build) void {
+    const cmd = b.addSystemCommand(&[_][]const u8{
+        "rm",
+        "-rf",
+        "zig-out",
+        "zig-cache",
+    });
+
+    const clean_step = b.step("clean", "Remove project artifacts");
+    clean_step.dependOn(&cmd.step);
 }
